fe56f9258e4c473c1070a99c2f8b545e004f8442
MohamedLoum1001 <mloum137@gmail.com>
2026-03-16 16:08:04 +0100

    deploy: add azure publish profile and update api url

--- src-front/src/App.tsx
+++ src-front/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://job-manager-api-dkb7cvcvavfqc5cp.francecentral-01.azurewebsites.net";
 
 interface Job {
   id: string;
